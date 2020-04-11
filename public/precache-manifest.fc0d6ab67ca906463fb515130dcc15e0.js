835f61be6dfdd618d23065d00fbeb8efa877054b
Mubashir Reshamwala <mubashir.reshamwala@speedboxapp.com>
2020-04-11 19:31:00 +0530

    added compiled react files

---
+++ public/precache-manifest.fc0d6ab67ca906463fb515130dcc15e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8669a1c7c18fd15777c8609a8b092787",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e50b22cd58362ec4c998",
+    "url": "/static/css/main.cc76feb3.chunk.css"
+  },
+  {
+    "revision": "07222b9ab3159d2dbbaf",
+    "url": "/static/js/2.7cb2fa0b.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.7cb2fa0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e50b22cd58362ec4c998",
+    "url": "/static/js/main.39a1bedc.chunk.js"
+  },
+  {
+    "revision": "02f3c6025a140a73e7c0",
+    "url": "/static/js/runtime-main.356358ba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
